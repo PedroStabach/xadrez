d759e58297966b5163c3afe78268706dfac81ac8
Pedro Stabach <pedroaugustostabach3@gmail.com>
2025-04-11 17:06:53 -0300

    tabela inicial pronta

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ const posicao = {
     "0" : "A", "1" : "B", "2" : "C", "3" : "D", "4" : "E", "5" : "F",
     "6" : "G", "7" : "H"
 }
-
+// TABELA INICIO
 for (let i = 0; i < 8; i++) {
   const linha = document.createElement("ul");
   linha.className = "linha";
--- class.js
+++ class.js
@@ -68,4 +68,3 @@ const pecasInfo = {
       }
     }
   };
-  
